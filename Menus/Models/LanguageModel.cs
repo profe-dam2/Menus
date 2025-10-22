7d1554c1ef0ef17a7613fc38e8ebab7a3a988094
damx <damx@erciapps.es>
2025-10-22 17:18:52 +0200

    Implementar lista de lenguajes de programacion

---
+++ Menus/Models/LanguageModel.cs
@@ -0,0 +1,10 @@
+﻿using Avalonia.Media.Imaging;
+
+namespace Menus.Models;
+
+public class LanguageModel
+{
+    public Bitmap ImagePath { get; set; }
+    public string Name { get; set; }
+    public LanguageModel(){}
+}
--- Menus/ViewModels/HelpViewModel.cs
+++ Menus/ViewModels/HelpViewModel.cs
@@ -1,5 +1,11 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using System;
+using System.Collections.ObjectModel;
+using Avalonia.Media.Imaging;
+using Avalonia.Platform;
+using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using HarfBuzzSharp;
+using Menus.Models;
 using Menus.Services;
 
 namespace Menus.ViewModels;
@@ -10,9 +16,15 @@ public partial class HelpViewModel:ViewModelBase
 
     [ObservableProperty] private int pageIndex=0;
     [ObservableProperty] private bool isReverse = false;
+
+    [ObservableProperty] private ObservableCollection<LanguageModel> selectedLanguages = new();
+    //Lista de lenguajes
+    [ObservableProperty] private ObservableCollection<LanguageModel> languageList = new();
+    
     public HelpViewModel(NavigationService navigationService)
     {
         this.navigationService = navigationService;
+        LoadLanguageList();
     }
 
     public HelpViewModel()
@@ -39,5 +51,40 @@ public partial class HelpViewModel:ViewModelBase
     public void NavigateTo(string tag_view)
     {
         navigationService.NavigateTo(tag_view);
+    }
+
+    private void LoadLanguageList()
+    {
+        var uri = new Uri("avares://Menus/Assets/Languages/python.png");
+        
+        LanguageList.Add(new LanguageModel(){ImagePath = new Bitmap(AssetLoader.Open(uri)), Name = "Python"});
+        
+        uri = new Uri("avares://Menus/Assets/Languages/c.png");
+        LanguageList.Add(new LanguageModel(){ImagePath = new Bitmap(AssetLoader.Open(uri)), Name = "C"});
+        
+        uri = new Uri("avares://Menus/Assets/Languages/cobol.png");
+        LanguageList.Add(new LanguageModel(){ImagePath = new Bitmap(AssetLoader.Open(uri)), Name = "Cobol"});
+        
+        uri = new Uri("avares://Menus/Assets/Languages/cplusplus.png");
+        LanguageList.Add(new LanguageModel(){ImagePath = new Bitmap(AssetLoader.Open(uri)), Name = "C++"});
+        
+        uri = new Uri("avares://Menus/Assets/Languages/csharp.png");
+        LanguageList.Add(new LanguageModel(){ImagePath = new Bitmap(AssetLoader.Open(uri)), Name = "C#"});
+        
+        uri = new Uri("avares://Menus/Assets/Languages/java.png");
+        LanguageList.Add(new LanguageModel(){ImagePath = new Bitmap(AssetLoader.Open(uri)), Name = "JAVA"});
+        
+        uri = new Uri("avares://Menus/Assets/Languages/javascript.png");
+        LanguageList.Add(new LanguageModel(){ImagePath = new Bitmap(AssetLoader.Open(uri)), Name = "JAVASCRIPT"});
+        
+        uri = new Uri("avares://Menus/Assets/Languages/kotlin.jpg");
+        LanguageList.Add(new LanguageModel(){ImagePath = new Bitmap(AssetLoader.Open(uri)), Name = "KOTLIN"});
+        
+        uri = new Uri("avares://Menus/Assets/Languages/sql.png");
+        LanguageList.Add(new LanguageModel(){ImagePath = new Bitmap(AssetLoader.Open(uri)), Name = "SQL"});
+
+        
+
+
     }
 }
